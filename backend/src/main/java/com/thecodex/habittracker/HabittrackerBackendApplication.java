aef50bf1027663742c5b79c7a4c44e437f435d70
the_codex_ <138112742+codingguru2221@users.noreply.github.com>
2026-02-16 20:56:07 +0530

    main coding structure added

---
+++ backend/src/main/java/com/thecodex/habittracker/HabittrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thecodex.habittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabittrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabittrackerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thecodex/habittracker/HabittrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecodex.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabittrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
